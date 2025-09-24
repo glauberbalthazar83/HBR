dfef140dc023489f55dcc0efec941865e79bed93
glauberbalthazar83 <glauber.balthazar@ifsp.edu.br>
2025-09-24 13:41:19 -0300

    Primeira versao do codigo.

---
+++ main.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI, Form
+from fastapi.requests import Request
+from fastapi.templating import Jinja2Templates
+
+app_web = FastAPI()
+paginas = Jinja2Templates(directory='HBR/paginas')
+
+@app_web.get('/')
+async def index(request: Request):
+    context = {
+        "request": request,
+        "resultado":0
+    }
+    return paginas.TemplateResponse('calculadora.html', context=context)
+
+@app_web.post('/calcular')
+async def login(request: Request):
+   
+   form = await request.form()
+   valor1 = float(form.get('valor1'))
+   operador = form.get('operador')
+   valor2 = float(form.get('valor2'))
+   resultado = 0.0
+
+   if(operador=="+"):
+       resultado = valor1 + valor2
+
+   if(operador=="-"):
+       resultado = valor1 - valor2
+
+   if(operador=="*"):
+       resultado = valor1 * valor2
+
+   if(operador=="/"):
+       resultado = valor1 / valor2     
+
+   contexto = {
+        "request": request,
+        "resultado": resultado
+    }
+   return paginas.TemplateResponse("calculadora.html", contexto)
