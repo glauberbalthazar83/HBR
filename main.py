215228aa0179e452a8bf6c8acd1d837a7e27b4c4
glauberbalthazar83 <glauber.balthazar@ifsp.edu.br>
2025-09-24 13:53:17 -0300

    Alterações de teste - add comentário

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from fastapi.templating import Jinja2Templates
 app_web = FastAPI()
 paginas = Jinja2Templates(directory='HBR/paginas')
 
+#acrescentando um comentário para atualizar o GitHub
+
 @app_web.get('/')
 async def index(request: Request):
     context = {
